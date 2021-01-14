277444e3b111076a476ece0369d50ade7662f12d
Rowan Amgad <rowan.amgad4@gmail.com>
2021-01-14 20:42:11 +0200

    accept modified

--- GUCera/Accept Course.aspx.cs
+++ GUCera/Accept Course.aspx.cs
@@ -14,7 +14,7 @@ namespace GUCera
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-          
+
         }
 
         [Obsolete]
@@ -26,57 +26,84 @@ namespace GUCera
             //create a new connection
             SqlConnection conn = new SqlConnection(connStr);
 
-            string courseName = TextBox1.Text.ToString() ;
-            
-            SqlCommand adminViewCoursestProc = new SqlCommand("AdminViewAllCourses", conn);
-            adminViewCoursestProc.CommandType = CommandType.StoredProcedure;
-
-           
-            SqlDataReader rdr;
-
-            
-
-            SqlCommand cmd = new SqlCommand("select * from Course where name='" + courseName +"'", conn);
-            cmd.CommandType = CommandType.Text;
-         
-            conn.Open();
-            rdr = cmd.ExecuteReader(CommandBehavior.SingleRow);
-            rdr.Read();
-            int cid =(int)  rdr.GetInt32(rdr.GetOrdinal("id"));
-            conn.Close();
-
-            int aid = (int)Session["field1"];
-            //int success =2 ;
-
-            SqlCommand adminAcceptProc = new SqlCommand("AdminAccRejExt", conn);
-            adminAcceptProc.CommandType = CommandType.StoredProcedure;
-
-            adminAcceptProc.Parameters.Add("@adminid", aid);
-            adminAcceptProc.Parameters.Add("@courseId", cid);
-            //adminAcceptProc.Parameters.Add("@success", success);
-            SqlParameter success = adminAcceptProc.Parameters.Add("@success", SqlDbType.Int);
-            success.Direction = ParameterDirection.Output;
-
-            conn.Open();
-            adminAcceptProc.ExecuteNonQuery();
-            conn.Close();
-            
-            if (success.Value.ToString().Equals("1"))
+            string courseName = TextBox1.Text.ToString();
+            if (courseName.Equals(""))
             {
-                Label1.Text = "You accepted a course";
+                Label1.Text = "Please enter a course name";
             }
             else
             {
-                if (success.Value.ToString().Equals("0"))
+                SqlCommand checkCourse = new SqlCommand("adminAccCheck", conn);
+                checkCourse.CommandType = CommandType.StoredProcedure;
+
+                checkCourse.Parameters.Add("@courseName", courseName);
+
+                SqlParameter exist = checkCourse.Parameters.Add("@exist", SqlDbType.Int);
+                exist.Direction = ParameterDirection.Output;
+
+                conn.Open();
+                checkCourse.ExecuteNonQuery();
+                conn.Close();
+
+                if (exist.Value.ToString().Equals("0"))
                 {
-                    Label1.Text = "This course is already accepted";
+                    Label1.Text = "This course doesn't exist";
+                }
+
+                else
+                {
+
+                    SqlCommand adminViewCoursestProc = new SqlCommand("AdminViewAllCourses", conn);
+                    adminViewCoursestProc.CommandType = CommandType.StoredProcedure;
+
+
+                    SqlDataReader rdr;
+
+
+
+                    SqlCommand cmd = new SqlCommand("select * from Course where name='" + courseName + "'", conn);
+                    cmd.CommandType = CommandType.Text;
+
+                    conn.Open();
+                    rdr = cmd.ExecuteReader(CommandBehavior.SingleRow);
+                    rdr.Read();
+                    int cid = (int)rdr.GetInt32(rdr.GetOrdinal("id"));
+                    conn.Close();
+
+                    int aid = (int)Session["field1"];
+                    //int success =2 ;
+
+                    SqlCommand adminAcceptProc = new SqlCommand("AdminAccRejExt", conn);
+                    adminAcceptProc.CommandType = CommandType.StoredProcedure;
+
+                    adminAcceptProc.Parameters.Add("@adminid", aid);
+                    adminAcceptProc.Parameters.Add("@courseId", cid);
+                    //adminAcceptProc.Parameters.Add("@success", success);
+                    SqlParameter success = adminAcceptProc.Parameters.Add("@success", SqlDbType.Int);
+                    success.Direction = ParameterDirection.Output;
+
+                    conn.Open();
+                    adminAcceptProc.ExecuteNonQuery();
+                    conn.Close();
+
+                    if (success.Value.ToString().Equals("1"))
+                    {
+                        Label1.Text = "You accepted a course";
+                    }
+                    else
+                    {
+                        if (success.Value.ToString().Equals("0"))
+                        {
+                            Label1.Text = "This course is already accepted";
+                        }
+                    }
+                    //Response.Write("You accepted a course");
+                    // Response.Redirect("Admin Page.aspx", true);
+
+
+
                 }
             }
-            //Response.Write("You accepted a course");
-            // Response.Redirect("Admin Page.aspx", true);
-
-
-
         }
     }
 }
